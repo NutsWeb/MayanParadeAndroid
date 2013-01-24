b37bf1a99209df636db183b45f40d6f87ee894f8
Zabre <julio.zabre@nuts.mx>
2013-01-24 17:16:55 -0600

    Maps

---
+++ src/com/nuts/mayanparade/MapActivity.java
@@ -0,0 +1,15 @@
+package com.nuts.mayanparade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity  
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.view);	
+	}
+}
